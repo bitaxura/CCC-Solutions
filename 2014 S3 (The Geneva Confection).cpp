9f4eb58f95cd1627007a2e0d818a82126b56d330
bitaxura <142764261+bitaxura@users.noreply.github.com>
2024-02-11 16:57:11 -0500

    Update 2014 S3 (The Geneva Confection).cpp

--- 2014 S3 (The Geneva Confection).cpp
+++ 2014 S3 (The Geneva Confection).cpp
@@ -1,58 +1,46 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int main()
 {
-	int T = 0;
-	cin>>T;
+    int test_cases;
+    cin>>test_cases;
 
-	for(int i = 0; i < T; i++){
+    for (int t = 0; t < test_cases; t++) {
+        int num_cars;
+        cin>>num_cars;
 
-		int a = 0;
-		cin>>a;
+        vector<int> cars(num_cars), branch;
+        for (int i = 0; i < num_cars; i++) {
+            cin>>cars[i];
+        }
 
-		vector<int> cars(a), branch;
-		for(int j = 0; j < a; j++)
-        {
-            int x = 0;
-            cin>>x;
-			cars[j] = x;
-		}
-
-		for(int j = 1, current = 0; j <= a; j++){
-
-			if(branch.size() != 0)
-            {
-				if(branch[branch.size() - 1] == j)
-                {
-					branch.pop_back();
-					current = j;
-					continue;
-				}
-			}
-
-			for(int k = cars.size(); k >= 1; k--)
-            {
-				if(cars[k - 1] == j){
-					cars.pop_back();
-					current = j;
-					break;
-				}else{
-					branch.push_back(cars[k - 1]);
-					cars.pop_back();
-				}
-			}
-			if (current != j) 
-            {
-				break;
+        int current_car = 1;
+        bool possible = true;
+        while (!cars.empty() && possible) {
+            if (!branch.empty() && branch.back() == current_car) {
+                branch.pop_back();
+                current_car++;
+            } else if (!cars.empty() && cars.back() == current_car) {
+                cars.pop_back();
+                current_car++;
+            } else {
+                if (!cars.empty()) {
+                    branch.push_back(cars.back());
+                    cars.pop_back();
+                }
             }
-		}
+            if (branch.size() > num_cars) {
+                possible = false;
+            }
+        }
 
-		if(cars.size() == 0 && branch.size() == 0){
-			cout<<"Y"<<endl;
-		}else{
-			cout<<"N"<<endl;
-		}
-	}
+        if (possible) {
+            cout<<"Y"<<endl;
+        } else {
+            cout<<"N"<<endl;
+        }
+    }
 }
