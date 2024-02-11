0a95d063f9048383cad19a1c5a67783577893453
bitaxura <142764261+bitaxura@users.noreply.github.com>
2024-02-11 16:40:07 -0500

    Create 2020 S1.cpp

---
+++ 2020 S1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<pair<double, double>> data(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> data[i].first >> data[i].second;
+    }
+
+    sort(data.begin(), data.end());
+
+    vector<double> speed;
+
+    for (int i = 1; i < n; i++) {
+        double delta_t = data[i].first - data[i - 1].first;
+        double delta_d = data[i].second - data[i - 1].second;
+
+        if (delta_t == 0)
+            speed.push_back(0);
+        else{
+            double current_speed = (delta_d / delta_t);
+            speed.push_back(abs(current_speed));
+        }
+    }
+
+    sort(speed.begin(), speed.end(), greater<double>());
+
+    cout << speed[0]<<endl;
+}
