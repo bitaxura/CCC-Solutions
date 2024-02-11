8f502ad17df5459e11c5873bdff13e8ea9c0484d
bitaxura <142764261+bitaxura@users.noreply.github.com>
2024-02-11 17:32:43 -0500

    Update 2021 S1 (Crazy Fencing).cpp

--- 2021 S1 (Crazy Fencing).cpp
+++ 2021 S1 (Crazy Fencing).cpp
@@ -1,26 +1,26 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
+    int fences;
+    cin>>fences;
 
-    double height[n+1];
-    double width[n];
+    double height[fences+1];
+    double width[fences];
     
-    for (int i = 0; i<=n; i++)
+    for (int i = 0; i<=fences; i++)
     {
         cin>>height[i];
     }
-    for(int i =0; i<n; i++)
+    for(int i =0; i<fences; i++)
     {
         cin>>width[i];
     }
 
     double area = 0;
-    for(int i = 0; i<n; i++){
+    for(int i = 0; i<fences; i++){
         area += width[i]*(height[i]+height[i+1])/2;
     }
-    cout << fixed << area;
+    cout<<fixed<<setprecision(2)<<area;
 }
