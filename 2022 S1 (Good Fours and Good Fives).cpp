8388bce50642b49badbd93d1ea975516bfc5abb4
bitaxura <142764261+bitaxura@users.noreply.github.com>
2024-02-17 22:39:09 -0500

    Update 2022 S1 (Good Fours and Good Fives).cpp

--- 2022 S1 (Good Fours and Good Fives).cpp
+++ 2022 S1 (Good Fours and Good Fives).cpp
@@ -5,8 +5,10 @@ int main() {
     int n, counter = 0;
     cin>>n;
 
-    for(int i = 0; i*4<=n; i++)
-        if((n - 4*i) % 5 == 0)
+    for(int i = 0; i*4<=n; i++){
+        if((n - 4*i) % 5 == 0){
             counter++;
-    cout<<counter<<endl;
+        }
+    }
+    cout<<counter;
 }
