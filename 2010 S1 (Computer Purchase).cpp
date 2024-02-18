46a79105deceaeb64ce33dea9c645c73a25a0b56
bitaxura <142764261+bitaxura@users.noreply.github.com>
2024-02-17 22:29:35 -0500

    Update 2010 S1 (Computer Purchase).cpp

--- 2010 S1 (Computer Purchase).cpp
+++ 2010 S1 (Computer Purchase).cpp
@@ -2,16 +2,14 @@
 #include <map>
 using namespace std;
 
-int main()
-{
-    pair<string, int> spec1 ("", 0);
-    pair<string, int> spec2 ("", 0);
+int main(){
+    pair<string, int> spec1;
+    pair<string, int> spec2;
 
     int n;
     cin>>n;
 
-    for (int i = 0; i<n; i++)
-    {
+    for (int i = 0; i<n; i++){
     string name;
     int r, s, d, total;
 
@@ -22,13 +20,11 @@ int main()
 
     pair<string, int> spec3 (name, total);
 
-    if (spec3.second > spec1.second || (spec3.second == spec1.second && spec1.first[0] > name[0])) 
-    {
+    if (spec3.second > spec1.second || (spec3.second == spec1.second && spec1.first[0] > name[0])){
         spec2 = spec1;
         spec1 = spec3;
     } 
-    else if (spec3.second > spec2.second || (spec3.second == spec2.second && spec2.first[0] > name[0])) 
-    {
+    else if (spec3.second > spec2.second || (spec3.second == spec2.second && spec2.first[0] > name[0])){
         spec2 = spec3;
     }
     }
