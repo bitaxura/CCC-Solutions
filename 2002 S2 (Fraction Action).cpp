a66cc60c534af43c5a20ea73ded8113615e966d5
bitaxura <142764261+bitaxura@users.noreply.github.com>
2024-02-17 22:27:15 -0500

    Update 2002 S2 (Fraction Action).cpp

--- 2002 S2 (Fraction Action).cpp
+++ 2002 S2 (Fraction Action).cpp
@@ -7,20 +7,24 @@ int main(){
    int numerator, denominator;
    cin>>numerator>>denominator;
 
-   if (numerator == 0)
+   if (numerator == 0){
       cout<<0<<endl;
+   }
    else{
       int ans = __gcd(numerator, denominator);
-      if (numerator%denominator == 0)
+      if (numerator%denominator == 0){
          cout<<(numerator/ans)/(denominator/ans)<<endl;
+      }
       else{
          int quotient = numerator / denominator;
          int remainder = abs(numerator) % denominator;
 
-         if (quotient < 1)
+         if (quotient < 1){
             cout<<(remainder/ans)<<"/"<<(denominator/ans)<<endl;
-         else
+         }
+         else{
             cout<<quotient<<" "<<(remainder/ans)<<"/"<<(denominator/ans)<<endl;
+         }
       }
    }
 }
