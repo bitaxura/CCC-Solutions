ab8f65bac37ca2cbd91104f2af635d6923a8bfd6
bitaxura <142764261+bitaxura@users.noreply.github.com>
2024-02-17 22:23:13 -0500

    Update 2002 S2 (Fraction Action).cpp

--- 2002 S2 (Fraction Action).cpp
+++ 2002 S2 (Fraction Action).cpp
@@ -3,35 +3,24 @@
 #include <algorithm>
 using namespace std;
 
-int main() 
-{
+int main(){
    int numerator, denominator;
    cin>>numerator>>denominator;
 
    if (numerator == 0)
-   {
       cout<<0<<endl;
-   }
-   else 
-   {
+   else{
       int ans = __gcd(numerator, denominator);
       if (numerator%denominator == 0)
-      {
-      cout<<(numerator/ans)/(denominator/ans)<<endl;
-      }
-      else
-      {
+         cout<<(numerator/ans)/(denominator/ans)<<endl;
+      else{
          int quotient = numerator / denominator;
          int remainder = abs(numerator) % denominator;
 
          if (quotient < 1)
-         {
-         cout<<(remainder/ans)<<"/"<<(denominator/ans)<<endl;
-         }
+            cout<<(remainder/ans)<<"/"<<(denominator/ans)<<endl;
          else
-         {
-         cout<<quotient<<" "<<(remainder/ans)<<"/"<<(denominator/ans)<<endl;
-         }
+            cout<<quotient<<" "<<(remainder/ans)<<"/"<<(denominator/ans)<<endl;
       }
    }
 }
